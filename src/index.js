1293f98d7ae992485caa74c9c75b99f93f037bc9
张富文 <48916435+zhangfuwen1989@users.noreply.github.com>
2025-02-20 09:47:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker01.bugaoxing.com"]: dockerHub,
+  ["d01.bugaoxing.com"]: dockerHub,
   ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
   ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
   ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
